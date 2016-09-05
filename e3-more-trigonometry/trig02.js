2b58b9bcdb4e2fff6d2a97d4fcbb8c5d84200bc6
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2016-09-05 22:43:59 +0300

    episode 3 - simple moving dot

---
+++ e3-more-trigonometry/trig02.js
@@ -0,0 +1,53 @@
+window.onload = function() {
+	var canvas = document.getElementById('canvas'),
+			context = canvas.getContext('2d'),
+			width = canvas.width = window.innerWidth,
+			height = canvas.height = window.innerHeight;
+
+			// Centering The Object on the canvas
+			// point to place the object according to y axis
+	var centerY = height * .5,
+			// point to place the object according to x axis
+			centerX = width * .5,
+			
+			// How far to move the object inside canvas
+			// Amplitude
+			offset = height * .3,
+			
+			// How fast to move the object back and fouth
+			// How fast we incrementing the angle so that it
+			// 		moves from 0 to 2PI
+			// Frequency
+			speed = 0.1,
+
+			// This angle we will put in the sin function
+			// 		to create an animation of moving dot
+			angle = 0;
+
+	// call render function to draw an object
+	render();
+
+	function render() {
+
+		// Calculating y for moving verticaly
+		var y = centerY + Math.sin(angle) * offset;
+
+		context.clearRect(0, 0, width, height);
+		// Begin the path to draw a circle
+		context.beginPath();
+
+		// Creating a circle
+		context.arc(centerX, y, 40, 0, Math.PI * 2, false);
+
+		// Fill the circle
+		context.fill();
+
+		// Increase angle with some speed
+		angle += speed;
+
+		// Animate with requestAnimationFrame function
+		requestAnimationFrame(render);
+	}
+};
+
+
