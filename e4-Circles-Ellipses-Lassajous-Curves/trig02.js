ca0c3887491f489cfa0406b68ce7918b0dfeea4b
Yaroslav Prodanchuk <yprodanchuk@gmail.com>
2016-09-10 18:02:14 +0300

    Using sin for moving horizontally

---
+++ e4-Circles-Ellipses-Lassajous-Curves/trig02.js
@@ -0,0 +1,35 @@
+window.onload = function() {
+	var canvas = document.getElementById('canvas'),
+			context = canvas.getContext('2d'),
+			width = canvas.width = window.innerWidth,
+			height = canvas.height = window.innerHeight;
+
+	var centerY = height * .5,
+			centerX = width * .5,
+			offset = height * .4,
+			speed = 0.1,
+			angle = 0;
+
+	render();
+
+	function render() {
+
+		var x = centerX + Math.sin(angle) * offset;
+
+		context.clearRect(0, 0, width, height);
+		context.beginPath();
+
+		// moving horizontally
+		context.arc(x, centerY, 40, 0, Math.PI * 2, false);
+
+		context.fill();
+
+		angle += speed;
+
+		requestAnimationFrame(render);
+	}
+
+
+};
+
+
