3a494374be2e7e87fc66f4faa49e9bc3478fd91c
Dan <dan@dan.com>
2019-12-22 21:15:45 +0200

    Learned about vectors

---
+++ e6-vectors/main.js
@@ -0,0 +1,25 @@
+// Creating vector 1
+
+// We have vector in the global scope
+let v1 = vector.create(10, 5)
+
+
+// let v2 = vector.create(3, 4),
+// 		v3 = v1.add(v2);
+
+// console.log(v1.getX())
+// console.log(v1.getY())
+// console.log(v1.getAngle())
+// console.log(v1.getLength())
+
+// v1.setAngle(Math.PI / 6);
+// v1.setLength(100);
+
+console.log(v1.getLength())
+let v2 = v1.multiply(2)
+console.log(v2.getLength())
+
+
+
+
+
